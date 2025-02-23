03d678b2099137f9b69d5b7fe99fc8e4aba6f74a
unknown <examp23.lo23@gmail.com>
2025-02-23 18:16:35 +0800

    add animation

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+    const musicEl = document.querySelector("#music");
+    const clickEl = document.querySelector("#click");
+    const letterEl = document.querySelector(".letter");
+
+    function playMusic(){
+      musicEl.play();
+    }
+
+    clickEl.addEventListener("click", () => {
+      letterEl.classList.toggle("flap");
+    });
+
+    letterEl.addEventListener("click", () => {
+      letterEl.classList.toggle("flap");
+    });
+
+    // clickEl.addEventListener("click", playMusic);
 });
