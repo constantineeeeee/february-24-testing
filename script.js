920af413249d638c8b66e41da9c7b7a961876f16
unknown <examp23.lo23@gmail.com>
2025-02-23 23:58:23 +0800

    close to final changes

--- script.js
+++ script.js
@@ -2,18 +2,49 @@ document.addEventListener("DOMContentLoaded", () => {
     const musicEl = document.querySelector("#music");
     const clickEl = document.querySelector("#click");
     const letterEl = document.querySelector(".letter");
+    const balloonEl = document.querySelector(".balloon");
+    const tobi1El = document.querySelector(".tobi1");
+    const tofi1El = document.querySelector(".tofi");
+    const tobi2El = document.querySelector(".tobi2");
+    const usPicEl = document.querySelector(".us");
 
-    function playMusic(){
+    // function playMusic(){
       musicEl.play();
-    }
+    // }
 
     clickEl.addEventListener("click", () => {
+      musicEl.play();
       letterEl.classList.toggle("flap");
+      balloonEl.classList.toggle("flapBal");
     });
 
     letterEl.addEventListener("click", () => {
+      letterCheck = false;
       letterEl.classList.toggle("flap");
+      clickEl.checked = false;
     });
 
-    // clickEl.addEventListener("click", playMusic);
+
+    usPicEl.addEventListener("click", () => {
+      letterEl.classList.toggle("flap");
+      usPicEl.classList.toggle("picFlap");
+    });
+    
+    tobi1El.addEventListener("click", () => {
+      letterEl.classList.toggle("flap");
+      tobi1El.classList.toggle("picFlap");
+    });
+    
+    tobi2El.addEventListener("click", () => {
+      letterEl.classList.toggle("flap");
+      tobi2El.classList.toggle("picFlap");
+    });
+
+    
+    tofi1El.addEventListener("click", () => {
+      letterEl.classList.toggle("flap");
+      tofi1El.classList.toggle("picFlap");
+    });
+
+    clickEl.addEventLstener("click", playMusic);
 });
